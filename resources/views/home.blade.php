6aec1b5712a3415fbf194048ac9ee2ddbfe4e1e3
Morgan Kpassi <>
2024-06-29 22:00:08 +0200

    fix color home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,23 +23,23 @@
     </div>
 </section>
 <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-                <defs>
-                    <path id="gentle-wave"
-                        d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                </defs>
-                <g class="parallax">
-                    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(0,64,255,0.7)" />
-                    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(0,64,255,0.5)" />
-                    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(0,64,255,0.3)" />
-                    <use xlink:href="#gentle-wave" x="48" y="7" fill="#0040FF" />
-                </g>
-    </svg>
+    viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+    <defs>
+        <path id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+    </defs>
+    <g class="parallax">
+        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(0,64,255,0.7)" />
+        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(0,64,255,0.5)" />
+        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(0,64,255,0.3)" />
+        <use xlink:href="#gentle-wave" x="48" y="7" fill="#0040FF" />
+    </g>
+</svg>
 {{-- UGC --}}
-<section class="bg-blue-700">
+<section class="bg-[#0040FF]">
     <div class="container mx-auto lg:py-32 py-16 px-4 lg:px-0">
         <div class="w-full flex items-center justify-center sm:flex-row flex-col gap-10 z-[200] relative">
-            <div class="lg:w-1/3 bg-blue-700">
+            <div class="lg:w-1/3 bg-[#0040FF]">
                 <h2 class="text-white text-2xl py-3 uppercase font-bold">L'ugc ?</h2>
                 <p class="text-white text-base py-3">UGC signifie User Generated Content, en français c'est le contenu généré par 
                 les utilisateurs</p>
